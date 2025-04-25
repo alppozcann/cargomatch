cf673f0334885312698ba91ba98ff5198341728c
alppozcann <62207565+alppozcann@users.noreply.github.com>
2025-04-25 20:48:58 +0300

    modified controller

--- ilan2-proje/app/Http/Controllers/DashboardController.php
+++ ilan2-proje/app/Http/Controllers/DashboardController.php
@@ -15,20 +15,12 @@ public function index()
 {
     $user = auth()->user();
 
-<<<<<<< HEAD
-    if ($user->isYukVeren()) {
-=======
     if ($user->user_type === 'yukveren') {
->>>>>>> 9523c88 (initial commit2)
         $yukler = $user->yukler()->with('matchedGemiRoute')->latest()->get();
         return view('dashboard.yukveren', compact('user', 'yukler'));
     }
 
-<<<<<<< HEAD
-    if ($user->isGemiSahibi()) {
-=======
     if ($user->user_type === 'gemici') {
->>>>>>> 9523c88 (initial commit2)
         $rotalar = $user->gemiRoutes()->with('matchedYukler')->latest()->get();
         return view('dashboard.gemisahibi', compact('user', 'rotalar'));
     }
